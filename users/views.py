1d52e87d675a04bee629936b149d92830e574938
islam duishobaev <dujsobaevislam01@gmail.com>
2023-09-08 18:18:31 +0600

    dz 7

--- users/views.py
+++ users/views.py
@@ -14,8 +14,6 @@ from rest_framework.viewsets import ModelViewSet, generics
 #     serializer.save()
 #     return Response(data=serializer.data, status=201)
 '''registration_user -> cvb -> MODELVIEWSET'''
-
-
 class Registration_userViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = serializers.UserRegistrationSerializer(data=request.data)
@@ -38,9 +36,9 @@ class Registration_userViewSet(ModelViewSet):
 #         return Response(data={"message": "User successfully confirmed."}, status=200)
 #     except ConfirmationCode.DoesNotExist:
 #         return Response(data={"error": "Invalid confirmation code."}, status=400)
-'''confirm_user_api_view -> cbv -> '''
 
 
+'''confirm_user_api_view -> cbv -> generics.CreateAPIView'''
 class confirm_userViewPost(generics.CreateAPIView):
     def post(self, request, *args, **kwargs):
         code = request.data.get('code')
@@ -68,8 +66,6 @@ class confirm_userViewPost(generics.CreateAPIView):
 #     return Response(status=401, data={'error': 'Invalid credentials'})
 
 '''login_user_api_view -> cvb -> MODELVIEWSET '''
-
-
 class login_userViewSet(ModelViewSet):
     def post(self, request, *args, **kwargs):
         serializer = serializers.UserLoginSerializer(data=request.data)
